2788a093b6e6f90ce26153f88aa66960cbec5a90
Akiko <yoshinariakiko@gmail.com>
2015-06-24 01:15:22 +0000

    renamed courses.jp

--- app/controllers/instructor/courses_controller.rb
+++ app/controllers/instructor/courses_controller.rb
@@ -1,4 +1,5 @@
 class Instructor::CoursesController < ApplicationController
+  
   before_action :authenticate_user!
   before_action :require_authorized_for_current_course, :only => [:show]
   
--- app/controllers/lessons_controller.rb
+++ app/controllers/lessons_controller.rb
@@ -1,9 +1,11 @@
 class LessonsController < ApplicationController
+  before_action :authenticate_user!
   def show
     
   end
 
   private
+
   helper_method :current_lesson
 
   def current_lesson
