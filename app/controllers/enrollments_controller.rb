41107f1f96968d93a721e25192ea4b575ed3ff8f
yoshinaria <yoshinariakiko@gmail.com>
2015-07-17 17:07:34 -0500

    added stripe payment

--- app/controllers/enrollments_controller.rb
+++ app/controllers/enrollments_controller.rb
@@ -2,8 +2,29 @@ class EnrollmentsController < ApplicationController
   before_action :authenticate_user!
 
   def create
-    current_user.enrollments.create(:course => current_course)
-    redirect_to course_path(current_course)
+    if current_course.premium?
+     # Amount in cents
+      @amount = 500
+
+      customer = Stripe::Customer.create(
+        :email => current_user.email,
+        :card  => params[:stripeToken]
+      )
+
+      charge = Stripe::Charge.create(
+        :customer    => customer.id,
+        :amount      => @amount,
+        :description => 'Flixter Premo Content',
+        :currency    => 'usd'
+      )
+    end
+    
+  current_user.enrollments.create(:course => current_course)
+  redirect_to course_path(current_course)
+
+  rescue Stripe::CardError => e
+    flash[:error] = e.message
+    redirect_to root_path
   end
 
   private
--- app/controllers/lessons_controller.rb
+++ app/controllers/lessons_controller.rb
@@ -1,10 +1,22 @@
 class LessonsController < ApplicationController
   before_action :authenticate_user!
+  before_action :require_enrollment_for_lesson, :only => [:show]
+
   def show
     
   end
 
   private
+
+  def require_enrollment_for_lesson
+    this_course = current_lesson.section.course
+
+    if current_user.enrolled_in?(this_course)
+      return true
+    else
+      redirect_to course_path(this_course), alert: 'You need to be enrolled in this course to view lessons'
+    end
+  end
 
   helper_method :current_lesson
 
